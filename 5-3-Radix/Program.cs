05a0203cf824bd51712bbe8441a65d3f80fdfdfd
Karla110297 <dinorakarla@hotmail.com>
2018-12-02 12:42:22 -0800

    radix
    radix

--- 5-3-Radix/Program.cs
+++ 5-3-Radix/Program.cs
@@ -10,6 +10,33 @@ namespace _5_3_Radix
     {
         static void Main(string[] args)
         {
+            //arreglos a ordenar
+            int[] Arreglo1 = { 3, 6, 9, 5, 1, 4, 7, 2, 1, 3 };
+            int[] Arreglo2 = { 8, 3, 9, 3, 11, 7, 1, 27, 12 };
+            int[] Arreglo3 = { 10, 40, 36, 5, 24, 2, 5, 8 };
+            int[] Arreglo4 = { 55, 42, 0, -3, 0, -1, 2, 4, 7 };
+            int[] Arreglo5 = { 25, 108, 1024, 12, 351, 251, 39 };
+
+            Radix rs = new Radix();//crea la clase            
+
+            rs.Imprimir(Arreglo1);//manda imprimir elementos de arreglo 1
+            Console.WriteLine();
+
+            rs.Imprimir(Arreglo2);//manda imprimir elementos de arreglo 2
+            Console.WriteLine();
+
+            rs.Imprimir(Arreglo3);//manda imprimir elementos de arreglo 3
+            Console.WriteLine();
+
+            rs.Imprimir(Arreglo4);//manda imprimir elementos de arreglo 4
+            Console.WriteLine();
+
+            rs.Imprimir(Arreglo5);//manda imprimir elementos de arreglo 5
+            Console.WriteLine();
+
+
+            //Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
---
+++ 5-3-Radix/Radix.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_3_Radix
+{
+    class Radix
+    {
+        public Radix() { }
+
+
+
+        public void Imprimir(int[] X)
+        {
+            for (int i = 0; i < X.Length; i++)//imprime arreglo sin ordenar
+            {
+                Console.Write(X[i] + ", ");
+            }
+            radixSort(X);//manda llamar funcion radixSort para ordenar
+            Console.WriteLine("\nOrdenado");//imprime arreglo ordenado
+            for (int j = 0; j < X.Length; j++)
+            {
+                Console.Write("{0} ", X[j]);
+            }
+            Console.ReadLine();
+        }
+
+        
+        public void radixSort(int[] arreglo)
+        {
+            int[] t = new int[arreglo.Length]; // Arreglo auxiliar .
+
+            int r = 2;// Tamaño en bits de nuestro grupo. 
+
+            int b = 32; // Número de bits de un entero en c#. 
+
+           
+
+            int[] count = new int[1 << r];            // Inicia el conteo a asignación de los arreglos.
+            int[] pref = new int[1 << r]; // Notar dimensiones 2^r el cual es el número de todos los  valores posibles de un número r-bit
+
+             
+            int groups = (int)Math.Ceiling((double)b / (double)r);// Número de grupos.
+
+            
+
+            int mask = (1 << r) - 1;// Máscara para identificar los grupos.
+
+            
+            for (int c = 0, shift = 0; c < groups; c++, shift += r)// Implementación del algoritmo 
+            {
+                
+                for (int j = 0; j < count.Length; j++)// Reiniciar el conteo en los arreglos.
+                    count[j] = 0;
+                
+                for (int i = 0; i < arreglo.Length; i++)                // Contar elementos del c-vo grupo.
+                    count[(arreglo[i] >> shift) & mask]++;
+
+
+                pref[0] = 0; // Calculando prefijos.
+
+                for (int i = 1; i < count.Length; i++)
+                    pref[i] = pref[i - 1] + count[i - 1];
+
+               
+
+                for (int i = 0; i < arreglo.Length; i++) // De a[] a t[] elementos ordenados por c-vo grupo .
+                    t[pref[(arreglo[i] >> shift) & mask]++] = arreglo[i];
+
+                
+
+                t.CopyTo(arreglo, 0);// a[]=t[] e inicia otra vez hasta el último grupo. 
+
+               
+            }
+
+            // Está ordenado 	   
+        }
+
+    }
+}
